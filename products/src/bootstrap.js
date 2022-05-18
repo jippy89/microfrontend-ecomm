130c0aa1dcac4c0587a7cd6808613f0b8415fb93
Jip Irfandy <irfandyjip98@gmail.com>
2022-05-18 18:39:42 +0700

    refactor: make component independent
    refactor: change the file exposure to `bootstrap`

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,30 @@
 import faker from 'faker'
 
-let products = ''
-
-for (let i=0; i < 3; i++) {
-  const name = faker.commerce.productName()
-  products += `<div>${name}</div>`
+const mount = el => {
+  let products = ''
+  
+  for (let i=0; i < 3; i++) {
+    const name = faker.commerce.productName()
+    products += `<div>${name}</div>`
+  }
+  
+  el.innerHTML = products
 }
 
-document.querySelector('#dev-products').innerHTML = products
+// Context/Situation: #1
+// We are running this file in development mode.
+// We are using local index.html file.
+// Which DEFINITELY has an element with an id of 'dev-products'.
+// We want to immediately render our app into that element.
+if (process.env.NODE_ENV === 'development') {
+  const el = document.getElementById('dev-products-products-only') 
+  if (el) {
+    mount(el)
+  }
+}
+// Context/Situation: #2
+// We are running the file in development or production mode.
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-products' exists.
+// WE DO NOT WANT to immediately render our app.
+export { mount }
