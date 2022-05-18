e48d67007a7806ee2411fd72b46cf9c6843693c0
Jip Irfandy <irfandyjip98@gmail.com>
2022-05-18 18:54:37 +0700

    refactor: update API for `cart` component

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,7 +1,27 @@
 import faker from 'faker'
 
-const cartText = `
-  <div>You have ${faker.random.number()} items in your cart</div>
-`
+const mount = el => {
+  const cartText = `
+    <div>You have ${faker.random.number()} items in your cart</div>
+  `
+  
+  el.innerHTML = cartText
+}
 
-document.querySelector('#dev-cart').innerHTML = cartText
+// Context/Situation: #1
+// We are running this file in development mode.
+// We are using local index.html file.
+// Which DEFINITELY has an element with an id of 'dev-products'.
+// We want to immediately render our app into that element.
+if (process.env.NODE_ENV === 'development') {
+  const el = document.getElementById('dev-cart-cart-only') 
+  if (el) {
+    mount(el)
+  }
+}
+// Context/Situation: #2
+// We are running the file in development or production mode.
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-products' exists.
+// WE DO NOT WANT to immediately render our app.
+export { mount }
